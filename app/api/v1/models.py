a5dd5447067a0313cffe135a8532dce2d1b50f31
sistemas <sistemas@localhost.localdomain>
2015-08-23 00:53:59 +0200

    remove postgresql specific funcionalities to allow using any database

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 from app import db
 import hashlib
-from flask.ext.sqlalchemy import  BaseQuery
-from sqlalchemy_searchable import SearchQueryMixin
-from sqlalchemy_searchable import make_searchable
-from sqlalchemy_utils.types import TSVectorType
-
-make_searchable(options={'regconfig': 'pg_catalog.spanish'})
-
-class DocumentQuery(BaseQuery, SearchQueryMixin):
-    pass
 
 class Document(db.Model):
-    query_class = DocumentQuery
     __tablename__ = 'documents'
 
     id             = db.Column(db.Integer, primary_key=True)
@@ -20,7 +10,6 @@ class Document(db.Model):
     url            = db.Column(db.Unicode(255), nullable=False)
     content        = db.Column(db.UnicodeText, nullable=False)
     content_hash   = db.Column(db.Unicode(40), nullable=False)
-    search_vector  = db.Column(TSVectorType('name', 'content'))
     __table_args__ = (db.Index('idx_documents_name_url','name','url',unique = True),) 
 
     def __init__(self,name,url,content, content_hash):
@@ -29,16 +18,5 @@ class Document(db.Model):
         self.content_hash = content_hash
         self.content = content
 
-#    @property
-#    def serialize(self):
-#        return {
-#            'id'            : self.id,
-#            'name'          : self.name,
-#            'url'           : self.url,
-#            'content'       : self.content,
-#            'content_hash'  : self.content_hash,
-#            'search_vector' : self.search_vector
-#        }
- 
     def __repr__(self):
         return '<id {} name {}>'.format(self.id, self.name)
--- load_test_data.py
+++ load_test_data.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from unidecode import unidecode
 from faker import Faker
 fake = Faker('es_ES')
 
@@ -7,12 +8,16 @@ conn = psycopg2.connect(os.environ['DATABASE_URL'])
 cur = conn.cursor()
 #cur.execute('create table test(t varchar(200));')
 
-for i in range (1000):
-    name = fake.name()
-    url = fake.uri()
-    content = fake.text()
-    content_hash = fake.name()
+
+with open('definiti.txt') as f:
+
+  for i in range(1000):
+    name = unidecode(fake.name()).casefold()
+    url = fake.url()
+    content = unidecode(fake.address()).casefold()
+    content_hash = 'teyeduedgwuedguwgd'
     cur.execute("INSERT INTO documents (name,content,url,content_hash) VALUES ('{}','{}','{}','{}')".format(name,content,url,content_hash));
+#    cur.execute("INSERT INTO test  VALUES ('{}')".format(i.strip()));
 
 conn.commit()
 conn.close()

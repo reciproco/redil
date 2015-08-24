134c197578547294f6f06249c60d40655e73d501
reciproco <reciproco@users.noreply.github.com>
2015-08-24 10:11:50 +0200

    redirect errors to stdout to make easier heroku debugging

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,16 @@
 from flask import Flask, make_response, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import sys
+import logging
 from app.errors import init_errors
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
     app.config.from_object(os.environ['APP_SETTINGS'])
     print(os.environ['APP_SETTINGS'])
     db.init_app(app)

6d4805529b55b048bce3519548f99271b01fd2cd
reciproco <reciproco@users.noreply.github.com>
2015-08-27 14:52:26 +0200

    testing with file upload

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from app import db
-from flask import Blueprint, jsonify, make_response, render_template
+from flask import Blueprint, jsonify, make_response, render_template, safe_join, request
+import io
 
 mod_web = Blueprint('web',__name__ ,url_prefix='/')
 
@@ -17,3 +18,23 @@ def method_not_allowed(error):
 def index():
     return render_template('index.html')
 
+@mod_web.route('upload', methods=['GET', 'POST'])
+def upload():
+
+    if request.method == 'POST':
+
+        b = io.BytesIO()
+
+        print(request.form)
+        print(dir(request.files['file1']))
+        b.write(request.files['file1'].stream.read())
+        b.seek(0)
+        
+        view = b.read(100)
+
+        print(view[:30])
+
+        
+#        filename =  safe_join(app.config["APPLICATION_ROOT"], path)
+
+    return render_template('upload.html')

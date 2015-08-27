f71ec609ecf0359d60c1c6caf9f7405968074191
sistemas <sistemas@localhost.localdomain>
2015-08-28 01:14:14 +0200

    testing githooks

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -16,6 +16,7 @@ def method_not_allowed(error):
 
 @mod_web.route('/', methods=['GET', 'POST'])
 def index():
+    print('PRUEBA GITHOOK')
     return render_template('index.html')
 
 @mod_web.route('upload', methods=['GET', 'POST'])

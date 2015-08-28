1d43fa09854e72212fe0ac415c20e2d1cbbe62c8
reciproco <reciproco@users.noreply.github.com>
2015-08-28 10:59:17 +0200

    remove sens data

--- load_test_data.py
+++ load_test_data.py
@@ -2,12 +2,10 @@ import os
 import psycopg2
 from unidecode import unidecode
 
-conn = psycopg2.connect('postgres://jwimexnlvgbllw:o_wLus3ozh00LEr1_3MBqV_AMo@ec2-54-227-255-240.compute-1.amazonaws.com:5432/dfbuq9u9fol693')
+conn = psycopg2.connect(os.getenv('DATABASE_URL'))
 cur = conn.cursor()
-#cur.execute('create table test(t varchar(200));')
 
-
-with open('/home/jsegura/5.txt') as f:
+with open('/home/sistemas/datos/1.txt') as f:
  
     name = 'despertar_hombre.txt'
     url = 'http://sdsfdfdfs.gfd'
